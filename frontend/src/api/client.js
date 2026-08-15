b90166fdea3aa569de3000ef263380e79cc5dbec
ankitbhad <ankitbhad90223@gmail.com>
2026-08-15 14:55:41 +0530

    prodution issue fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,9 @@
 // src/api/client.js
 import axios from "axios";
 
-console.log("Backend URL:", import.meta.env.BACKEND_URL);
+// console.log("Backend URL:", import.meta.env.BACKEND_URL);
 const client = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   headers: { "Content-Type": "application/json" },
 });
 
